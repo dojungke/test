d247bfcf360ae416dc8f4d95710f09810a9ad51e
dojungke <doridori2363@gmail.com>
2026-03-11 17:00:53 +0900

    Update ArtifactData.cs

--- ArtifactData.cs
+++ ArtifactData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 //202500532 박도현 컴퓨터공학과
 
+//유물 아이템 정보를 불러오기위한 것
 [CreateAssetMenu(fileName = "Artifact", menuName = "Scriptable Objects/Artifact")]
 public class ArtifactData : ScriptableObject
 {
@@ -14,3 +15,4 @@ public class ArtifactData : ScriptableObject
     public int Prize;
 
 }
+
